9c741f1d2ceeace504a8070117597328be6a877d
bio8oid <8bollod8@mail.co.uk>
2019-07-12 23:36:40 +0200

    Add actions file

---
+++ actions.js
@@ -0,0 +1,54 @@
+
+
+const initialState = {
+    comments: [],
+    users: []
+};
+
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+initialState.comments: [
+
+const ADD_COMMENT = 'ADD_COMMENT';
+
+
+{
+    type: ADD_COMMENT,
+    text: 'My first comment !'
+}
+
+
+const REMOVE_COMMENT = 'ADD_COMMENT';
+
+{
+    type: REMOVE_COMMENT,
+    id: 'comment.number' 
+}
+
+const EDIT_COMMENT = 'ADD_COMMENT';
+
+{
+    type: EDIT_COMMENT,
+    id: 'comment.number',
+    text: 'new comment', from: "old.comment"
+}
+
+const THUMB_UP_COMMENT = 'ADD_COMMENT';
+
+{
+    type: THUMB_UP_COMMENT,
+    text: 'My first comment !'
+}
+
+const THUMB_DOWN_COMMENT = 'ADD_COMMENT';
+
+{
+    type: THUMB_DOWN_COMMENT,
+    text: 'My first comment !'
+}
+
+]
