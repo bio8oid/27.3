dd6120e78e8bb7b389efaea81fb950f3bfae5095
bio8oid <8bollod8@mail.co.uk>
2019-07-12 23:54:16 +0200

    Update reducer files

--- actions.js
+++ actions.js
@@ -1,9 +1,4 @@
-
-
-const initialState = {
-    comments: [],
-    users: []
-};
+import uuid from uuid;
 
 export const ADD_COMMENT = 'ADD_COMMENT';
 export const REMOVE_COMMENT = 'REMOVE_COMMENT';
@@ -11,44 +6,58 @@ export const EDIT_COMMENT = 'EDIT_COMMENT';
 export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
 export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
-initialState.comments: [
 
-const ADD_COMMENT = 'ADD_COMMENT';
+const initialState = {
+    comments: [],
+    users: []
+};
 
+addComment = text => {
+    return {
+        type: ADD_COMMENT,
+        text: 'My first comment !'
+    }
+}
 
-{
-    type: ADD_COMMENT,
-    text: 'My first comment !'
+removeComment = text => {
+    return {
+        type: REMOVE_COMMENT,
+        id: uuid.v4()
+    }
+}
+
+editComment = text => {
+    return {
+        type: EDIT_COMMENT,
+        id: uuid.v4(),
+        text: 'new comment', from: "old comment"
+    }
+}
+
+thumbUpComment = text => {
+    return {
+        type: THUMB_UP_COMMENT,
+        id: uuid.v4(),
+        votes: ++ 1
+    }
+}
+
+thumbDownComment = text => {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id: uuid.v4(),
+        votes: -- 1
+    }
 }
 
 
-const REMOVE_COMMENT = 'ADD_COMMENT';
+/*
+dispatch(addComment('nowy komentarz!'));
+dispatch(addComment('kolejny nowy komentarz!'));
 
-{
-    type: REMOVE_COMMENT,
-    id: 'comment.number' 
-}
+const boundAddComment = text => dispatch(addComment(text));
 
-const EDIT_COMMENT = 'ADD_COMMENT';
+boundAddComment('nowy komentarz!');
+boundAddComment('kolejny nowy komentarz!');
 
-{
-    type: EDIT_COMMENT,
-    id: 'comment.number',
-    text: 'new comment', from: "old.comment"
-}
-
-const THUMB_UP_COMMENT = 'ADD_COMMENT';
-
-{
-    type: THUMB_UP_COMMENT,
-    text: 'My first comment !'
-}
-
-const THUMB_DOWN_COMMENT = 'ADD_COMMENT';
-
-{
-    type: THUMB_DOWN_COMMENT,
-    text: 'My first comment !'
-}
-
-]
+*/
